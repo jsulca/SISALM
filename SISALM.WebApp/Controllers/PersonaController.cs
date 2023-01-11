7218fb4d0dfb21f000c2df5992f9cea307dc682e
Angel Alata <ang4732@gmail.com>
2023-01-10 23:16:30 -0500

    Se reviso la propiedad viewbag

--- SISALM.WebApp/Controllers/PersonaController.cs
+++ SISALM.WebApp/Controllers/PersonaController.cs
@@ -9,17 +9,28 @@ namespace SISALM.WebApp.Controllers
         {
             return View();
         }
+          public IActionResult Index02()
+        {
+            return Ok(new
+            {
+                mensaje="new"
+            });
+        }
 
         public IActionResult Nuevo()
         {
+            string mensaje = "Prospecto";
+            ViewBag.Mensaje = mensaje;
+
             PersonaModel model = new() 
             {
-                Nombre = "Junior",
-                Apellido = "Sulca",
+              Nombre = "Junior",
+                //Apellido = "Sulca",
                 Activo = true,
                 Nacimiento = DateTime.Today
             };
             return View(model);
         }
+
     }
 }
